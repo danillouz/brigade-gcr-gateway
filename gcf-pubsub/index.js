fd3e2b26d94d4da3cb26fa6a5208d10f5002b215
Daniël Illouz <daniel.illouz@me.com>
2018-03-20 12:28:34 +0100

    Update cloud function payload and docs

--- gcf-pubsub/index.js
+++ gcf-pubsub/index.js
@@ -5,57 +5,70 @@ const config = require('./config');
 
 exports.handle_gcr_events = event => {
   /*
-    event: {
-      context: {
-        eventId: '78938124563234',
-        timestamp: '2018-03-15T21:06:11.814Z',
-        eventType: 'google.pubsub.topic.publish',
-        resource: {
-          service: 'pubsub.googleapis.com',
-          name: 'projects/some-project/topics/gcr',
-          type: 'type.googleapis.com/google.pubsub.v1.PubsubMessage'
+    https://cloud.google.com/functions/docs/writing/background
+
+    ```json
+    {
+      "event": {
+        "context": {
+          "eventId": "78938124563234",
+          "timestamp": "2018-03-15T21:06:11.814Z",
+          "eventType": "google.pubsub.topic.publish",
+          "resource": {
+            "service": "pubsub.googleapis.com",
+            "name": "projects/some-gke-project/topics/gcr",
+            "type": "type.googleapis.com/google.pubsub.v1.PubsubMessage"
+          }
+        },
+        "data": {
+          "@type": "type.googleapis.com/google.pubsub.v1.PubsubMessage",
+          "attributes": {},
+          "data": "yJhYp24isdkdksldkekdjffkdsQWeOiJJTlNFUlQiL="
         }
-      },
-      data: {
-        '@type': 'type.googleapis.com/google.pubsub.v1.PubsubMessage',
-        attributes: {},
-        data: 'yJhYp24isdkdksldkekdjffkdsQWeOiJJTlNFUlQiL='
       }
     }
+    ```
   */
 
   const msg = event.data;
   const data = Buffer.from(msg.data, 'base64').toString();
 
-  console.log('data: ', data);
-
   /*
-    data: {
-      "action":"INSERT",
-      "digest":"gcr.io/some-project/hello-world@sha256:a9666ju9c8c59d3eec4998740c4rt642672983c71b65ad5539828f4bfgt62823",
-      "tag":"gcr.io/some-project/hello-world:s00vg6u"
+    https://cloud.google.com/container-registry/docs/configuring-notifications
+
+    ```json
+    {
+      "data": {
+        "action":"INSERT",
+        "digest":"gcr.io/some-gke-project/some-image-named@sha256:some-hash",
+        "tag":"gcr.io/some-gke-project/some-image-name:some-tag"
+      }
     }
+    ```
+
+    An action will either be "INSERT" or "DELETE".
   */
 
   // FIXME
   const url = `${config.brigade_gcr_gateway}/webhook/${config.org}/${config.repo}/${config.commit}`;
-
-  console.log('url: ', url);
 
   const options = {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json'
     },
-    body: {
-      context: event.context,
-      data
-    }
+    body: JSON.stringify({
+      gcrContext: event.context,
+      imageData: data
+    })
   };
 
   return fetch(url, options)
     .then(res => {
       if (!res.ok) {
+        console.error('res status code: ', res.status);
+        console.error('res status text: ', res.statusText);
+
         throw new Error(res.statusText);
       }
 
